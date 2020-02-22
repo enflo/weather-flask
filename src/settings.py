8f598dd009a3b1a3ce0c10a166386f5765dd28fa
Antoni Florit Homar <toniflorithomar@gmail.com>
2020-02-22 09:35:25 +0100

    parametrize mongo url

--- src/settings.py
+++ src/settings.py
@@ -1,7 +1,9 @@
 import os
 
+mongurl = os.environ.get('mongodb://51.158.125.197:27017/', 'mongodb://mongo:27017/')
+
 setMongo = {
-    "url":"mongodb://mongo:27017/",
+    "url": mongurl,
     "dbname":"weather",
     "dbcolweather":"weather",
     "dbcolusers":"users"
